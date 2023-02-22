2f47b3dd1cce6cf80dde9ac0db84ed32091e82b7
Bianca Aspin <bianca@mj94.net>
2023-02-22 09:15:13 -0800

    Refactor line function, clean out commented code.

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -3,10 +3,10 @@
 katz_deli = []
 
 def line(katz_deli):
-    line_readout = "The line is currently:"
     if len(katz_deli) == 0:
         print("The line is currently empty.")
     else:
+        line_readout = "The line is currently:"
         for i in range(1, len(katz_deli) + 1):
             index = i - 1
             line_readout += f" {i}. {katz_deli[index]}"
@@ -23,12 +23,5 @@ def now_serving(katz_deli):
     else:
         print(f"Currently serving {katz_deli[0]}.")
         katz_deli.pop(0)
-
-    # i = len(katz_deli)
-    # while i > 0:
-    #     head_of_line = katz_deli.pop(0)
-    #     print(f"Currently serving {head_of_line}.")
-    #     i -= 1
-    
 
 """ ipdb.set_trace() """
